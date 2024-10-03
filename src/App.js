b78aaec10df50476074874cd886610102a7a06b0
Saikiran5000 <saikiranreddygatla@gmail.com>
2024-10-03 11:07:36 +0530

    counter Reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state,input){
+    switch (input.type){
+      case "DEC":
+        return state+input.payload;
+       case "RESET":
+        return 0 ;
+        case "INC":
+          return state+input.payload 
+        default: return state
+    }
 }
+
+
+function App(){
+
+   const [counter,dispatch]= useReducer(reducer,0)
+
+
+  return(
+    <>
+    <button onClick={()=>{
+      dispatch({type:"DEC",payload:-1})
+    }} className="btn btn-primary">-</button>
+    <h1>SAIKIRAN {counter}</h1>
+    <button className="btn btn-primary" onClick={()=>{
+      dispatch({type:"INC",payload:1})
+    }}>+</button>
+    <button className="btn btn-danger" onClick={()=>{
+      dispatch({type:"RESET"})
+    }}>Reset</button>
+    
+    </>
+  )
+}
+
+
+
+
+
+
+
 
 export default App;
